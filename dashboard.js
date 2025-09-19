dec0285518b7a6363855de3f817799f05e646ab6
jakob.roessler@stud.uni-regensburg.de <jakobs3mini@gmail.com>
2025-09-19 11:33:26 +0200

    State gameState to check if there is a game ongoing

--- dashboard.js
+++ dashboard.js
@@ -3,19 +3,42 @@ const timer_stop_button = document.getElementById('timer-stop-button');
 const add_point_button = document.getElementById('add-point-button');
 const reset_point_button = document.getElementById('reset-point-button');
 const timerDisplay = document.querySelector('#game-timer p');
+const scoreboard = document.querySelector('#game-scoreboard p');
+const activetest = document.querySelector('#game-canvas p');
 
 
 
 
 
 // Timer functionality
-
 let varGameTime = 5;
 let timer;
 const totalDurationInMs = varGameTime * 1000;
 
+
+// ActiveGame state
+const gameState = {
+  active: false
+};
+
+const gameStatusProxy = new Proxy(gameState, {
+  set(target, property, value) {
+    if (property === 'active') {
+      activetest.textContent = `Status: ${value ? 'Aktiv' : 'Inaktiv'}`;
+    }
+    target[property] = value;
+    return true;
+  }
+});
+
+
+
+
+// starting the countdown
 function startTimer() {
   if (timer) return;
+
+  gameStatusProxy.active = true;
   const startTime = Date.now();
 
   timer = setInterval(() => {
@@ -25,12 +48,16 @@ function startTimer() {
     if (timeLeftInMs <= 0) {
       clearInterval(timer);
       timer = null;
+      
+      gameStatusProxy.active = false;
 
       // make the color red when the timer hiits 0 seconds
       timerDisplay.style.color = 'red';
+      scoreboard.style.color = 'red';
       timerDisplay.textContent = `00:00`;
       setTimeout(() => {
         timerDisplay.style.color = 'black';
+        scoreboard.style.color = 'black';
       }, 500);
 
       return;
@@ -43,17 +70,20 @@ function startTimer() {
   }, 10);
 }
 
-
+// stop & reset
 function stopTimer() {
   clearInterval(timer);
   timer = null;
+
+  gameStatusProxy.active = false;
   timeLeftInMs = totalDurationInMs;
   timerDisplay.textContent = `${varGameTime}:00`;
 }
 
-
 timer_start_button.addEventListener('click', startTimer);
 timer_stop_button.addEventListener('click', stopTimer);
+gameStatusProxy.active = false;
+
 
 
 
@@ -61,10 +91,9 @@ timer_stop_button.addEventListener('click', stopTimer);
 
 let score = 0;
 let pointsPerClick = 10;
-const scoreboard = document.querySelector('#game-scoreboard p');
-
 
 function collectPoints(){
+    //if (!varActiveGame) return;
     score += pointsPerClick;
     scoreboard.textContent = `${score} points`;
 }
